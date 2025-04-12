3ea007653df272a7d0ce92515e036da1eba3f83a
ImperialZebra <imperialzebra067@gmail.com>
2025-04-11 22:54:20 -0400

    demo 8 complete!

---
+++ demo_arbitration/quartus/part1/software/arbitration_part1_APP/obj/default/Arbitration_part1.d
@@ -0,0 +1,18 @@
+obj/default/Arbitration_part1.o: Arbitration_part1.c system.h \
+ ../arbitration_part1_APP_bsp/linker.h \
+ ../arbitration_part1_APP_bsp//HAL/inc/sys/alt_irq.h \
+ ../arbitration_part1_APP_bsp//HAL/inc/nios2.h \
+ ../arbitration_part1_APP_bsp//HAL/inc/alt_types.h \
+ ../arbitration_part1_APP_bsp/system.h
+
+system.h:
+
+../arbitration_part1_APP_bsp/linker.h:
+
+../arbitration_part1_APP_bsp//HAL/inc/sys/alt_irq.h:
+
+../arbitration_part1_APP_bsp//HAL/inc/nios2.h:
+
+../arbitration_part1_APP_bsp//HAL/inc/alt_types.h:
+
+../arbitration_part1_APP_bsp/system.h:
