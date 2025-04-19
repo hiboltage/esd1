80c1d931217b05b258a58615636db06e6f746a77
ImperialZebra <imperialzebra067@gmail.com>
2025-04-19 00:06:06 -0400

    programmed board, eclipse debug can't find it to connect

---
+++ demo_arbitration/quartus/part2/software/Arbitration_Nios0_APP/obj/default/arbitration_nios0.d
@@ -0,0 +1,18 @@
+obj/default/arbitration_nios0.o: arbitration_nios0.c system.h \
+ ../Arbitration_Nios0_APP_bsp/linker.h \
+ ../Arbitration_Nios0_APP_bsp//HAL/inc/sys/alt_irq.h \
+ ../Arbitration_Nios0_APP_bsp//HAL/inc/nios2.h \
+ ../Arbitration_Nios0_APP_bsp//HAL/inc/alt_types.h \
+ ../Arbitration_Nios0_APP_bsp/system.h
+
+system.h:
+
+../Arbitration_Nios0_APP_bsp/linker.h:
+
+../Arbitration_Nios0_APP_bsp//HAL/inc/sys/alt_irq.h:
+
+../Arbitration_Nios0_APP_bsp//HAL/inc/nios2.h:
+
+../Arbitration_Nios0_APP_bsp//HAL/inc/alt_types.h:
+
+../Arbitration_Nios0_APP_bsp/system.h:
