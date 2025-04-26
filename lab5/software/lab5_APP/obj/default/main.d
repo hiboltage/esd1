ab4700bc6c6ca80d358bfa874e565659e4a49561
ImperialZebra <imperialzebra067@gmail.com>
2025-04-26 01:35:57 -0400

    register load functional, gpio output untested

---
+++ lab5/software/lab5_APP/obj/default/main.d
@@ -0,0 +1,16 @@
+obj/default/main.o: main.c system.h ../lab5_APP_bsp/linker.h \
+ ../lab5_APP_bsp//HAL/inc/alt_types.h \
+ ../lab5_APP_bsp//HAL/inc/sys/alt_irq.h ../lab5_APP_bsp//HAL/inc/nios2.h \
+ ../lab5_APP_bsp/system.h
+
+system.h:
+
+../lab5_APP_bsp/linker.h:
+
+../lab5_APP_bsp//HAL/inc/alt_types.h:
+
+../lab5_APP_bsp//HAL/inc/sys/alt_irq.h:
+
+../lab5_APP_bsp//HAL/inc/nios2.h:
+
+../lab5_APP_bsp/system.h:
